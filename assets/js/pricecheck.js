d57634512ffe6cfde49516724f2e8e6034d0220f
Kurohyou <scaseydv@gmail.com>
2022-10-17 16:38:03 -0600

    Basic search working

--- assets/js/pricecheck.js
+++ assets/js/pricecheck.js
@@ -1,30 +1,53 @@
-const getPrices = async (e)=>{
+const headerIndexes = {
+  'Item':-1,
+  'GP':-1,
+  'Rarity':-1,
+  'Source':-1,
+  'Page':-1,
+  'Type':-1,
+  'Attunement':-1
+}
+const itemHeaders = Object.keys(headerIndexes);
+let itemContent;
+const getPriceData = async (e)=>{
   let response;
   try {
     // Fetch first 10 files
     response = await gapi.client.sheets.spreadsheets.values.get({
       spreadsheetId: '14LkK1PrfkjeTsqpHypRrRKctvIM4DZ1Dag939D8wKig',
-      range:'Full!A6:C'
+      range:'Full'
     });
-    console.log(response);
   } catch (err) {
     console.error(err);
     return;
   }
-  // const range = response.result;
-  // // Flatten to string to display
-  // const headers = range.values[0];
-  
+  const sheetHeaders = response.result.values.shift();
+  itemHeaders.forEach(h => headerIndexes[h] = sheetHeaders.indexOf(h));
+  console.log('headerIndexes',headerIndexes);
+  const headerEntries = Object.entries(headerIndexes);
+  itemContent = response.result.values.map(arr => 
+    headerEntries.reduce((memo,[head,i])=>{
+      memo[head] = arr[i];
+      return memo;
+    },{})
+  );
+  console.log('itemContent',itemContent);
+  const searchForm = document.getElementById('item-search');
+  if(searchForm){
+    [...searchForm.children].forEach(el => el.removeAttribute('disabled'));
+  }
+  const resultHeadDiv = document.getElementById('result-header');
 };
 
+
+
 var priceInit = (src)=>{
-  console.log(`==== ${src} ====`);
-  console.log('gapiInited',gapiInited);
   if(!gapiInited){
     console.log('setting timeout');
     setTimeout(priceInit,500,'from timeout');
   }else{
-    getPrices();
+    console.log('Initializing price data');
+    getPriceData();
   }
 };
 
