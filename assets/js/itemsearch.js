dba7f19cfd37a84f01150bb8a208ea09a0d4deea
Kurohyou <scaseydv@gmail.com>
2022-10-18 11:47:11 -0600

    Added rarity, attunement, and type filters

--- assets/js/itemsearch.js
+++ assets/js/itemsearch.js
@@ -1,3 +1,8 @@
+const searchInput = document.getElementById('search-term');
+const attuneFilter = document.getElementById('attune');
+const rareFilter = document.getElementsByName('rarity');
+const typeFilter = document.getElementsByName('type');
+
 const searchForm = document.getElementById('item-search');
 const resultTarget = document.getElementById('result-content');
 const outputDetails = async (arr) => {
@@ -8,10 +13,46 @@ const outputDetails = async (arr) => {
   )
 };
 
-const filterItems = (event) => {
-  const searchString = event.target.value;
-  console.log('searchString',searchString)
-  const filtered = fuzzysort.go(searchString,itemContent,{keys:['Item','GP','Rarity','Source','Type']});
+const getAllSelected = (select) => 
+  select && select.options ?
+    [...select.options].reduce((memo,opt) => {
+      if(opt.selected){
+        memo.push(opt.value);
+      }
+      return memo;
+    },[]) :
+    null;
+
+const getAllChecked = (checks) =>
+  checks ?
+    [...checks].reduce((memo,opt) => {
+      if(opt.checked){
+        memo.push(opt.value)
+      }
+      return memo;
+    },[]) :
+    null;
+
+const filterItems = () => {
+  const searchString = searchInput.value;
+  const attuneSearch = attuneFilter.value;
+  const rareSearch = getAllChecked(rareFilter);
+  const typeSearch = getAllChecked(typeFilter);
+  // debugger;
+  const filtered = fuzzysort.go(searchString,itemContent,{all:true,keys:['Item','GP','Source']})
+    .filter(item => {
+      const attuneMatch = attuneSearch ?
+        item.obj.Attunement === attuneSearch :
+        true;
+      const rarityMatch = rareSearch.length ?
+        rareSearch.indexOf(item.obj.Rarity) >= 0 :
+        true;
+      const typeMatch = typeSearch.length ?
+        typeSearch.some(t => item.obj.Type.includes(t)) :
+        true;
+      return attuneMatch && rarityMatch && typeMatch;
+    });
+    
   const newContent = filtered.reduce((memo,item) => {
     const searchString = item.obj.Item.replace(/\s*\(.+\)\s*/g,'').trim();
     memo.push(...templates.itemRow({...item.obj,r20Link:`https://roll20.net/compendium/dnd5e/${searchString}`,beyondLink:`https://www.dndbeyond.com/magic-items/${searchString.replace(/\s+/g,'-')}`}));
@@ -23,3 +64,11 @@ const filterItems = (event) => {
 
 searchForm.addEventListener('submit',(e)=> e.preventDefault);
 searchForm.addEventListener('input',filterItems);
+const filterInit = ()=>{
+  if(itemContent){
+    filterItems();
+  }else{
+    setTimeout(filterInit,500);
+  }
+};
+filterInit();
